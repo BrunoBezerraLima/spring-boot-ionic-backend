f27e83468c3e15822c1de498941dacd9b37590c2
Bruno Bezerra Lima <brunobezerralima@gmail.com>
2021-10-26 15:43:59 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/brunobezerra/cursomc/services/CategoriaService.java
+++ src/main/java/com/brunobezerra/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.brunobezerra.cursomc.domain.Categoria;
 import com.brunobezerra.cursomc.repositories.CategoriaRepository;
+import com.brunobezerra.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 
 	public Categoria burcar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
