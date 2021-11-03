424afda0350ee39003c30c8d53a81d1667e8ba22
Bruno Bezerra Lima <brunobezerralima@gmail.com>
2021-11-03 15:49:15 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/brunobezerra/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/brunobezerra/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.brunobezerra.cursomc.domain.Cliente;
 import com.brunobezerra.cursomc.domain.enums.TipoCliente;
 import com.brunobezerra.cursomc.dto.ClienteNewDTO;
+import com.brunobezerra.cursomc.repositories.ClienteRepository;
 import com.brunobezerra.cursomc.resources.exceptions.FieldMessage;
 import com.brunobezerra.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido!"));
 		}
 
+		Cliente aux = repository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente!"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
